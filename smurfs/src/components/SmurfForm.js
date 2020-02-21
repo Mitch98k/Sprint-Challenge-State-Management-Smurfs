cd98e138e5da914ae0a243ad778472b585163b36
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2020-02-21 13:06:39 -0700

    created the smurf list, add smurf form, and used context api.

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,69 +1,59 @@
 import React from 'react';
 import axios from 'axios';
-import {useContext} from 'react';
-import {FormContext} from '../contexts/FormContext';
-import '../index.css';
 
-class SmurfForm extends React.Component {
+export class SmurfForm extends React.Component {
     constructor() {
         super();
         this.state = {
-            smurfName: '',
-            SmurfAge: '',
-            smurfHeight: ''
+            name: '',
+            age: '',
+            height: ''
         };
     }
+    
 
-handleChanges = e => {
-    this.setState({
-        smurfName: e.target.value,
-        smurfAge: e.target.value,
-        smurfHeight: e.target.value
-    });
-};
+    handleChanges = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    };
 
-    handleSubmit(smurfName, smurfAge, smurfHeight) {
+    handleSubmit = (e) => {
+        e.preventDefault();
         axios
-            .post(
-                'http://localhost:3333/smurfs',
-                smurfName,
-                smurfAge,
-                smurfHeight
-            )
-            .then(res => {
-                console.log(res);
-            })
-            .catch(err => console.log(err));
-    };
-
+        .post('http://localhost:3333/smurfs', this.state)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    }
     render() {
-        return (
-            <div className = 'form'>
-                    <form onSubmit = {this.handleSubmit}>
-                        <input
-                        type = "text"
-                        name = "smurfName"
-                        value = {this.state.smurf}
-                        onChange = {this.handleChanges}
-                        />
-                        <input
-                        type = "text"
-                        name = "smurfAge"
-                        value = {this.state.smurf}
-                        onChange = {this.handleChanges}
-                        />
-                        <input
-                        type = "text"
-                        name = "smurfHeight"
-                        value = {this.state.smurf}
-                        onChange = {this.handleChanges}
-                        />
-                        <button>Create</button>
-                    </form>
-            </div>
-        );
-        }
-    };
-
-export default SmurfForm;
+    return (
+        <>
+        <form onSubmit = {this.handleSubmit}>
+            <input
+            type = 'text'
+            name = 'name'
+            placeholder = 'name'
+            value = {this.state.name}
+            onChange = {this.handleChanges}
+            />
+            <input
+            type = 'text'
+            name = 'age'
+            placeholder = 'age'
+            value = {this.state.age}
+            onChange = {this.handleChanges}
+            />
+            <input
+            type = 'text'
+            name = 'height'
+            placeholder = 'height'
+            value = {this.state.height}
+            onChange = {this.handleChanges}
+            />
+            <button>Add Smurf</button>
+        </form>
+        </>
+    )
+}
+}
    
